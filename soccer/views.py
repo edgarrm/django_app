eea826a64987ffae8aed3fd32a4f36c704abde33
Edgar R. Morales <edgarrealm@gmail.com>
2015-08-06 18:49:23 -0500

    erase comments

--- soccer/views.py
+++ soccer/views.py
@@ -1,27 +1,21 @@
-from models import Player, Team, Coach
 from rest_framework import viewsets
+from models import Player, Team, Coach
 from soccer.serializers import PlayerSerializer, TeamSerializer, CoachSerializer
 
 
 class PlayerViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
+    
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
 
 class TeamViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
+    
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
 
 class CoachViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
+    
     queryset = Coach.objects.all()
     serializer_class = CoachSerializer
