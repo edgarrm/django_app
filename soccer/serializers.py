d30947858ab3b915f0d0c68b78f614953c0da325
Edgar R. Morales <edgarrealm@gmail.com>
2015-08-06 18:51:21 -0500

    erase comments and spacing

--- soccer/serializers.py
+++ soccer/serializers.py
@@ -1,10 +1,10 @@
-from models import Player, Team, Coach
 from rest_framework import serializers
+from models import Player, Team, Coach
 
 
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    
     team_string = serializers.StringRelatedField(source = 'team.name')
-    #team = serializers.StringRelatedField()
 
     class Meta:
         model = Player
@@ -12,12 +12,14 @@ class PlayerSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
+    
     class Meta:
         model = Team
         fields = ('url', 'name', 'coach')
 
 
 class CoachSerializer(serializers.HyperlinkedModelSerializer):
+    
     class Meta:
         model = Coach
         fields = ('url', 'name')
