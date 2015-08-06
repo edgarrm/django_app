1d372317584ee57860051ad6daa97cbd62552dc1
Edgar <edgarrealm@gmail.com>
2015-08-05 19:36:48 -0500

    database

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'HOjqPeQWnb',
+        'PASSWORD': 'FT73IWBBQv',
         'HOST': 'localhost',
         'PORT': '',
     }
